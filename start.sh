e9e3af63e34198cfe5d5d71c337b884969b5279c
techlm77 <dominicyoungjackson@gmail.com>
2023-10-22 22:46:18 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 image_dir="/home/$(whoami)/Videos/animation"
 
 while true; do
-    for image in "$image_dir"/frame*.png; do
+    for image in "$image_dir"/*.png; do
         xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitorHDMI-0/workspace0/last-image -s "$image" # Change this line /backdrop/screen0/monitorHDMI-0/workspace0/last-image
         sleep 0.0416  # Delay for 1/24th of a second (24fps) or you can use 0.067 (15fps) or 0.033 (30fps)
     done
